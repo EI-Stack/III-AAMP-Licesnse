5321b911cd6f364fb93ba6febbf2ff59134921e6
root <cfleu198@gmail.com>
2018-05-04 04:07:03 +0000

    add read bin

--- python-s3-api.py
+++ python-s3-api.py
@@ -3,6 +3,7 @@
 import boto
 import boto.s3.connection
 import pandas as pd
+import numpy
 import os
 from flask import Flask
 from flask import request
@@ -43,8 +44,10 @@ def get_content():
     key = bucket.get_key(FILE_NAME)
     key.get_contents_to_filename(FILE_NAME)
 
-    # read file and translate it to JSON formate
-    df_file = pd.read_csv(FILE_NAME)
+    # read bin file and translate it to JSON formate
+    bin_data = numpy.fromfile(FILE_NAME, dtype='>d')
+    df_file = pd.DataFrame(data = bin_data)
+    #df_file = pd.read_csv(FILE_NAME)
 
     # delete file which stored in local
     os.remove(FILE_NAME)
