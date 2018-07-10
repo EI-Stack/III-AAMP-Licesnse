431002c06f3de690b330d721bbc41e1861c5e130
root <cfleu198@gmail.com>
2018-07-10 08:34:47 +0000

    add multiple target response

--- python-s3-api.py
+++ python-s3-api.py
@@ -78,37 +78,59 @@ def get_content():
     # read bin file and translate it to JSON formate
     #bin_data = numpy.fromfile(FILE_NAME, dtype='>d')
     #df_file = pd.DataFrame(data = bin_data)
-    df_file = convert_bin(FILE_NAME)
+    df_file_mean = convert_bin(FILE_NAME, 'mean')
+    df_file_max = convert_bin(FILE_NAME, 'max')
+    df_file_min = convert_bin(FILE_NAME, 'min')
 
     # delete file which stored in local
     os.remove(FILE_NAME)
 
     # load 'data' and 'index' in bin file, and append it into a list
     # follow data format from Grafana: https://github.com/grafana/simple-json-datasource/blob/master/README.md
-    jsonobj = json.loads(df_file.to_json(orient='split'))
-    datapoints_array = []
-    for i in range(0, jsonobj['index'][-1]):
-        datapoints_array.append([jsonobj['data'][i][0], jsonobj['index'][i]])
+    jsonobj_mean = json.loads(df_file_mean.to_json(orient='split'))
+    jsonobj_max = json.loads(df_file_max.to_json(orient='split'))
+    jsonobj_min = json.loads(df_file_min.to_json(orient='split'))
+
+    datapoints_array_mean = []
+    datapoints_array_max = []
+    datapoints_array_min = []
+    for i in range(0, jsonobj_mean['index'][-1]):
+        datapoints_array_mean.append([jsonobj_mean['data'][i][0], jsonobj_mean['index'][i]])
+        datapoints_array_max.append([jsonobj_max['data'][i][0], jsonobj_max['index'][i]])
+        datapoints_array_min.append([jsonobj_min['data'][i][0], jsonobj_min['index'][i]])
 
     #TODO: group data by mean (or preprocess before data upload to S3
     # construct json array for responding
-    dict_data = {}
-    dict_data["target"] = FILE_NAME
-    dict_data["datapoints"] = datapoints_array
-    jsonarr = json.dumps([dict_data])
+    dict_data_mean = {}
+    dict_data_mean["target"] = 'mean'
+    dict_data_mean["datapoints"] = datapoints_array_mean
+    dict_data_max = {}
+    dict_data_max["target"] = 'max'
+    dict_data_max["datapoints"] = datapoints_array_max
+    dict_data_min = {}
+    dict_data_min["target"] = 'min'
+    dict_data_min["datapoints"] = datapoints_array_min
+
+    jsonarr = json.dumps([dict_data_mean, dict_data_max, dict_data_min])
 
     return str(jsonarr)
     #return str(df_file.index.values)
 
 
-def convert_bin (filename):
+def convert_bin (filename, pd_type):
     bytes_read = open(filename, "rb").read()
     size = [hexint(bytes_read[(i*4):((i+1)*4)]) for i in range(2)]
     signal = [struct.unpack('f',bytes_read[(i*4):((i+1)*4)]) for i in range(2,2+size[0]*size[1])]
     data = np.array(signal).reshape(size)
     return_df = pd.DataFrame(data = data)
     return_df = return_df.T
-    return_df = return_df.groupby(np.arange(len(return_df))//256).mean()
+
+    if pd_type == 'mean':
+        return_df = return_df.groupby(np.arange(len(return_df))//4096).mean()
+    elif pd_type == 'max':
+        return_df = return_df.groupby(np.arange(len(return_df))//4096).max()
+    else:
+        return_df = return_df.groupby(np.arange(len(return_df))//4096).min()
 
     #print(len(return_df))
 
