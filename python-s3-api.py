c1c96fd47c80ab86ad2bb12678bbd306042f0dfe
owen <cfleu198@gmail.com>
2019-02-23 11:11:49 +0800

    remove max, min and disable aggregate

--- python-s3-api.py
+++ python-s3-api.py
@@ -56,7 +56,7 @@ def get_content():
     ID_TAG = jsonobj['tag']
     S3_PATH = jsonobj['date']
     SAMPLE_RATE = 8192
-    DISPLAY_POINT = 256
+    DISPLAY_POINT = 65536
 
 
     # establish connection between blob storage and this client app
@@ -71,8 +71,9 @@ def get_content():
     bucket = s3_connection.get_bucket(BUCKET_NAME, validate=False)
 
     # goto bucket and get file accroding to the file name
-    PATH_DEST = ID_MACHINE + '/' + ID_TAG + '/' + S3_PATH + '/'
+    PATH_DEST = 'base/' + ID_MACHINE + '/' + ID_TAG + '/' + S3_PATH + '/'
     s3_bin_data = os.path.join(PATH_DEST, FILE_NAME)
+    print(s3_bin_data)
     key = bucket.get_key(s3_bin_data)
 
     try:
@@ -81,20 +82,28 @@ def get_content():
         return 'File not found'
 
     # get metadata (timestamp)
-    bucket = s3_connection.get_bucket(BUCKET_NAME, validate=False)
-    key = Key(bucket)
-    remote_key = bucket.get_key(s3_bin_data)
-    key_timestamp = remote_key.metadata
-    key_timestamp = key_timestamp['ts']
+    #bucket = s3_connection.get_bucket(BUCKET_NAME, validate=False)
+    #key = Key(bucket)
+    #remote_key = bucket.get_key(s3_bin_data)
+    #key_timestamp = remote_key.metadata
+    #key_timestamp = key_timestamp['ts']
     #print('timestamp = ' + key_timestamp['ts'])
     
     # read bin file and translate it to JSON formate
-    df_file_mean, file_length = convert_bin(FILE_NAME, 'mean', DISPLAY_POINT)
-    df_file_max, file_length = convert_bin(FILE_NAME, 'max', DISPLAY_POINT)
-    df_file_min, file_length = convert_bin(FILE_NAME, 'min', DISPLAY_POINT)
+    #df_file_mean, file_length = convert_bin(FILE_NAME, 'mean', DISPLAY_POINT)
+    #df_file_max, file_length = convert_bin(FILE_NAME, 'max', DISPLAY_POINT)
+    #df_file_min, file_length = convert_bin(FILE_NAME, 'min', DISPLAY_POINT)
+    df_file, file_length = convert_bin(FILE_NAME, DISPLAY_POINT)
 
     # calculate start-time and end-time
     #TIME_START = datetime.datetime.fromtimestampint(int(key_timestamp)).strftime('%Y-%m-%d %H:%M:%S')
+
+    date_list = date = s3_bin_data.split("/")
+    date = date_list[3] + '-' + date_list[4] + '-' + date_list[5]
+    time_list = s3_bin_data.split("/")[6].split("-")
+    time = time_list[3] + ':' + time_list[3] + ':' + time_list[5].split("_")[0]
+    key_timestamp = date + ' ' + time
+
     TIME_START = int(datetime.datetime.strptime(key_timestamp, '%Y-%m-%d %H:%M:%S').strftime('%s')) * 1000
     TIME_DELTA = file_length * 1000 / SAMPLE_RATE / DISPLAY_POINT
     print ('TIME_DELTA=', TIME_DELTA)
@@ -104,42 +113,41 @@ def get_content():
 
     # load 'data' and 'index' in bin file, and append it into a list
     # follow data format from Grafana: https://github.com/grafana/simple-json-datasource/blob/master/README.md
-    jsonobj_mean = json.loads(df_file_mean.to_json(orient='split'))
-    jsonobj_max = json.loads(df_file_max.to_json(orient='split'))
-    jsonobj_min = json.loads(df_file_min.to_json(orient='split'))
+    jsonobj_mean = json.loads(df_file.to_json(orient='split'))
+    #jsonobj_max = json.loads(df_file_max.to_json(orient='split'))
+    #jsonobj_min = json.loads(df_file_min.to_json(orient='split'))
 
     datapoints_array_mean = []
-    datapoints_array_max = []
-    datapoints_array_min = []
+    #datapoints_array_max = []
+    #datapoints_array_min = []
     for i in range(0, DISPLAY_POINT):
-        #datapoints_array_mean.append([jsonobj_mean['data'][i][0], jsonobj_mean['index'][i]])
-        #datapoints_array_max.append([jsonobj_max['data'][i][0], jsonobj_max['index'][i]])
-        #datapoints_array_min.append([jsonobj_min['data'][i][0], jsonobj_min['index'][i]])
         datapoints_array_mean.append([jsonobj_mean['data'][i][0], TIME_START])
-        datapoints_array_max.append([jsonobj_max['data'][i][0], TIME_START])
-        datapoints_array_min.append([jsonobj_min['data'][i][0], TIME_START])
+        #datapoints_array_max.append([jsonobj_max['data'][i][0], TIME_START])
+        #datapoints_array_min.append([jsonobj_min['data'][i][0], TIME_START])
 
         TIME_START = str(int(TIME_START) + TIME_DELTA)
 
 
     # construct json array for API response
     dict_data_mean = {}
-    dict_data_mean["target"] = 'mean'
+    dict_data_mean["target"] = 'original'
     dict_data_mean["datapoints"] = datapoints_array_mean
-    dict_data_max = {}
-    dict_data_max["target"] = 'max'
-    dict_data_max["datapoints"] = datapoints_array_max
-    dict_data_min = {}
-    dict_data_min["target"] = 'min'
-    dict_data_min["datapoints"] = datapoints_array_min
+    #dict_data_max = {}
+    #dict_data_max["target"] = 'max'
+    #dict_data_max["datapoints"] = datapoints_array_max
+    #dict_data_min = {}
+    #dict_data_min["target"] = 'min'
+    #dict_data_min["datapoints"] = datapoints_array_min
 
-    jsonarr = json.dumps([dict_data_mean, dict_data_max, dict_data_min])
+    #jsonarr = json.dumps([dict_data_mean, dict_data_max, dict_data_min])
+    jsonarr = json.dumps([dict_data_mean])
 
     return str(jsonarr)
     #return str(df_file.index.values)
 
 
-def convert_bin (filename, pd_type, DISPLAY_POINT):
+#def convert_bin (filename, pd_type, DISPLAY_POINT):
+def convert_bin (filename, DISPLAYPOINT):
     bytes_read = open(filename, "rb").read()
     size = [hexint(bytes_read[(i*4):((i+1)*4)]) for i in range(2)]
     signal = [struct.unpack('f',bytes_read[(i*4):((i+1)*4)]) for i in range(2,2+size[0]*size[1])]
@@ -148,14 +156,14 @@ def convert_bin (filename, pd_type, DISPLAY_POINT):
     return_df = return_df.T
     file_length = len(return_df)
 
-    length = file_length / DISPLAY_POINT
+    length = file_length / DISPLAYPOINT
 
-    if pd_type == 'mean':
-        return_df = return_df.groupby(np.arange(len(return_df))/length).mean()
-    elif pd_type == 'max':
-        return_df = return_df.groupby(np.arange(len(return_df))/length).max()
-    else:
-        return_df = return_df.groupby(np.arange(len(return_df))/length).min()
+    #if pd_type == 'mean':
+    #    return_df = return_df.groupby(np.arange(len(return_df))/length).mean()
+    #elif pd_type == 'max':
+    #    return_df = return_df.groupby(np.arange(len(return_df))/length).max()
+    #else:
+    #    return_df = return_df.groupby(np.arange(len(return_df))/length).min()
 
     #print(len(return_df))
 
