6900968ca137840506abf35b80c3bc7d1b558996
Owen HT Lu <cfleu198@gmail.com>
2019-03-14 02:19:25 +0800

    Update python-s3-api.py

--- python-s3-api.py
+++ python-s3-api.py
@@ -112,7 +112,7 @@ def get_content():
 
     #TIME_START = int(datetime.datetime.strptime(key_timestamp, '%Y-%m-%d %H:%M:%S').strftime('%s')) * 1000
     TIME_START = int(datetime.datetime.strptime(DATE, '%Y/%m/%d').strftime('%s')) * 1000
-    TIME_DELTA = float(float(file_length / SAMPLE_RATE) / DISPLAY_POINT) * 1000
+    TIME_DELTA = float(float(BIN_LENGTH / SAMPLE_RATE) / DISPLAY_POINT) * 1000
     #TIME_DELTA = 0.1220703125
     print ('TIME_START', TIME_START)
 
