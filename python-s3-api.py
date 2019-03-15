9b46540ba4c4ea1cd361d85a1f71b94f798c3599
Owen HT Lu <cfleu198@gmail.com>
2019-03-16 00:10:58 +0800

    Update python-s3-api.py

--- python-s3-api.py
+++ python-s3-api.py
@@ -49,7 +49,7 @@ def get_content():
     date_obj = jsonobj['range']['from']
     #date_obj = date_obj.split('T')[0]
     
-    DATE = datetime.datetime.strptime(date_obj, '%Y-%m-%dT%H:%M:%S.000Z')
+    DATE = datetime.datetime.strptime(date_obj, '%Y-%m-%dT%H:%M:%S.%fZ')
     DATE = DATE + datetime.timedelta(hours=8)
     DATE = DATE.strftime('%Y-%m-%d')
 
